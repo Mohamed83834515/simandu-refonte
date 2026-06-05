2860784f60c9babda93470b6d2373f5b6e2f3ca7
Mohamed83834515 <mohamedmoustaphatr@gmail.com>
2026-06-05 17:01:36 +0000

    test push

--- src/routes/_authenticated/index.tsx
+++ src/routes/_authenticated/index.tsx
@@ -2,5 +2,5 @@ import { createFileRoute } from '@tanstack/react-router'
 import { Dashboard } from '@/simadou/allfonctionalities/dashboard'
 
 export const Route = createFileRoute('/_authenticated/')({
-  component: Dashboard,
+  component: Dashboard, 
 })
