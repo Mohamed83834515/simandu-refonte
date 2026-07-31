deccdb975fe07c61357d493b89e45fd56f46cfbd
DiMalle <boubacarmalle86@gmail.com>
2026-07-31 19:02:26 +0000

    feat: add new route for Suivi du PAO and update sidebar navigation

--- src/routes/_authenticated/suivi-resultats/suivi-ptba/index.tsx
+++ src/routes/_authenticated/suivi-resultats/suivi-ptba/index.tsx
@@ -1,22 +1,21 @@
-
 import { PageRouteLayout } from '@/Global/HookRoute/genericRoute'
 import ListeSuiviPtba from '@/simadou/allfonctionalities/suivi-ptba/ListeSuiviPtba'
 import { createFileRoute } from '@tanstack/react-router'
 import { Eye } from 'lucide-react'
 
 export const Route = createFileRoute(
-    '/_authenticated/suivi-resultats/suivi-ptba/',
+  '/_authenticated/suivi-resultats/suivi-ptba/',
 )({
-    component: RouteComponent,
+  component: RouteComponent,
 })
 
 function RouteComponent() {
-    return (
-        <PageRouteLayout
-            title="Suivi du Plan d'Action Operationnel"
-            icon={Eye}
-            showAddButton={false}
-            listComponent={ListeSuiviPtba}
-        />
-    )
+  return (
+    <PageRouteLayout
+      title="Suivi du Plan d'Action Operationnel"
+      icon={Eye}
+      showAddButton={false}
+      listComponent={ListeSuiviPtba}
+    />
+  )
 }
